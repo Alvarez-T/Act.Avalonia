f179c3c80d3a7b75515fd06f189daed0da028cb5
Thiago Alvarez <thiago.alvarez2012@hotmail.com>
2024-08-05 19:12:35 -0300

    Namespace changes

--- Act.Avalonia.UI/AssemblyInfo.cs
+++ Act.Avalonia.UI/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 [assembly: Guid("3d250460-0547-4eaf-b9c6-19fdbc7d9729")]
-[assembly: XmlnsDefinition("https://github.com/Alvarez-T/Act.Avalonia", "Dotfy.Avalonia.UI")]
-[assembly: XmlnsDefinition("https://github.com/Alvarez-T/Act.Avalonia", "Dotfy.Avalonia.UI.Controls")]
-[assembly: XmlnsDefinition("https://github.com/Alvarez-T/Act.Avalonia", "Dotfy.Avalonia.UI.TemplatedControls")]
-[assembly: XmlnsDefinition("https://github.com/Alvarez-T/Act.Avalonia", "Dotfy.Avalonia.UI.Converters")]
+[assembly: XmlnsDefinition("https://github.com/Alvarez-T/Act.Avalonia", "Act.Avalonia.UI")]
+[assembly: XmlnsDefinition("https://github.com/Alvarez-T/Act.Avalonia", "Act.Avalonia.UI.Controls")]
+[assembly: XmlnsDefinition("https://github.com/Alvarez-T/Act.Avalonia", "Act.Avalonia.UI.TemplatedControls")]
+[assembly: XmlnsDefinition("https://github.com/Alvarez-T/Act.Avalonia", "Act.Avalonia.UI.Converters")]
